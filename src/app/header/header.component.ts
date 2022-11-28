894ff614a5c3f25071fb0306d3f63e205d2211d4
tej0897 <tej897@gmail.com>
2022-11-28 20:00:15 +0530

    save before auth

--- src/app/header/header.component.ts
+++ src/app/header/header.component.ts
@@ -1,7 +1,8 @@
 import { Component, OnInit } from '@angular/core';
 import { Router } from '@angular/router';
-import { UserAuthService } from '../services/user-auth.service';
-import { UserService } from '../services/user.service';
+import { UserAuthService } from '../_services/user-auth.service';
+import { UserService } from '../_services/user.service';
+
 
 @Component({
   selector: 'app-header',
--- src/app/login/login.component.ts
+++ src/app/login/login.component.ts
@@ -1,8 +1,9 @@
 import { Component, OnInit } from '@angular/core';
 import { NgForm } from '@angular/forms';
 import { Router } from '@angular/router';
-import { UserAuthService } from '../services/user-auth.service';
-import { UserService } from '../services/user.service';
+import { UserAuthService } from '../_services/user-auth.service';
+import { UserService } from '../_services/user.service';
+
 
 @Component({
   selector: 'app-login',
